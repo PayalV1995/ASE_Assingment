aeaba80cb10850ce4d2ce7a6327822755a92794b
Payal Vaghani <sutariya.payal5@gmail.com>
2024-01-04 16:40:39 +0000

    Start to implement shapecontrol class

--- WindowsFormsApp1/shapeControl.cs
+++ WindowsFormsApp1/shapeControl.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace WindowsFormsApp1
 {
-    internal class shapeControl
+    internal class shapeControl : canvass
     {
+        public void runCommands(String strtxt)
+        {
+            string errMsg = string.Empty;
+            string strCommand = string.Empty;
+            Boolean runFlg = true;
+            int cmdX = 0, cmdY = 0, cmdz = 0;
+            string[] arrCommand = strtxt.ToLower().Split(new string[] { ";" }, StringSplitOptions.None);
+            string[] oneCommand;
+        }
+
     }
 }
