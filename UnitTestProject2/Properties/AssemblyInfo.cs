6176dd6d488149b9a782f265a15bace3dc0a8667
Payal Vaghani <sutariya.payal5@gmail.com>
2024-01-05 17:48:59 +0000

    Completed UnitTest

---
+++ UnitTestProject2/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("UnitTestProject2")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8cee5143-a7f3-47bf-96f8-b6bbcd0474b4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
