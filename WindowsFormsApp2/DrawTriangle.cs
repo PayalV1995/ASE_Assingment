89c90974eca991bbf0c3cea6f4a405f307522df1
Payal Vaghani <sutariya.payal5@gmail.com>
2024-01-05 12:19:25 +0000

    implemented drawtriangle class

--- WindowsFormsApp2/DrawTriangle.cs
+++ WindowsFormsApp2/DrawTriangle.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace WindowsFormsApp2
 {
-    internal class DrawTriangle
+    public class DrawTriangle : shapes
     {
+        private Point[] pnt;
+        public DrawTriangle(Point[] pnt)
+        {
+
+            this.pnt = pnt;
+        }
+        public DrawTriangle() { }
+
+        public override void setTriangle(int x, int y, Point[] points)
+        {
+            base.set(x, y);
+            this.pnt = points;
+        }
+        public override void Draw(Graphics g, Pen pen, Brush brush)
+        {
+
+            g.DrawPolygon(pen, pnt);
+            g.FillPolygon(brush, pnt);
+        }
     }
 }
